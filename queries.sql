212654b7e91ee22b92e2ad8cee2a1ca62a350ff3
joey <josephanthonynguyen@gmail.com>
2022-03-14 20:39:24 -0500

    tests failing

--- queries.sql
+++ queries.sql
@@ -1,7 +1,17 @@
 -- Part 1: Test it with SQL
 
+DESCRIBE job;
+
 -- Part 2: Test it with SQL
+
+SELECT name FROM employer WHERE (location = "St. Louis City");
 
 -- Part 3: Test it with SQL
 
+DROP TABLE job;
+
 -- Part 4: Test it with SQL
+
+SELECT * FROM skill
+WHERE id IN (SELECT skills_id FROM job_skills WHERE skills_id IS NOT NULL)
+ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,44 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location required.")
+    @Size(max = 50, message = "Location entry is too long. Try a shorter term.")
+    private String location;
 
+    @JoinColumn
+    @OneToMany
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer(){
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
